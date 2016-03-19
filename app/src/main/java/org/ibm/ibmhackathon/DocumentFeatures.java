919f2b77a8c71f9ef0e659408cdecba28cf963ec
shubham7stark <shubhamk.goyal2525@gmail.com>
2016-03-19 17:53:15 +0530

    Configured Alchemy Api for KeyExtraction from Text

--- app/src/main/java/org/ibm/ibmhackathon/DocumentFeatures.java
+++ app/src/main/java/org/ibm/ibmhackathon/DocumentFeatures.java
@@ -1,7 +1,20 @@
 package org.ibm.ibmhackathon;
 
+import java.util.Vector;
+
 /**
  * Created by manthan on 19/3/16.
  */
 public class DocumentFeatures {
+
+    String status;
+    String language;
+    String text;
+    Vector<Keywords> keywords;
+
+    public class Keywords{
+        String text;
+        String relevance;
+    }
+
 }
--- app/src/main/java/org/ibm/ibmhackathon/MainActivity.java
+++ app/src/main/java/org/ibm/ibmhackathon/MainActivity.java
@@ -1,5 +1,6 @@
 package org.ibm.ibmhackathon;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.CharacterPickerDialog;
@@ -7,6 +8,21 @@ import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.alchemyapi.api.AlchemyAPI;
+import com.alchemyapi.api.AlchemyAPI_KeywordParams;
+import com.alchemyapi.api.AlchemyAPI_NamedEntityParams;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathExpressionException;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -18,6 +34,14 @@ public class MainActivity extends AppCompatActivity implements Callback<Document
 
     Button fileList;
     Retrofit retrofit;
+    TextView textView;
+    final static String AlchemyAPI_Key = "";
+
+    String text_to_parse = "There appears to be increased uncertainty surrounding the forthcoming referendum " +
+            "on UK membership of the European Union. That uncertainty is likely to have been a significant driver " +
+            "of the decline in sterling. It may also delay some spending decisions and depress growth in aggregate " +
+            "demand in the near term.";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +60,7 @@ public class MainActivity extends AppCompatActivity implements Callback<Document
         fileList.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                new GetFeatureTask().execute(null,null,null);
 
             }
         });
@@ -43,6 +68,7 @@ public class MainActivity extends AppCompatActivity implements Callback<Document
 
     private void uisetup() {
         fileList = (Button)findViewById(R.id.filelist);
+        textView = (TextView)findViewById(R.id.textView);
     }
 
     @Override
@@ -52,6 +78,65 @@ public class MainActivity extends AppCompatActivity implements Callback<Document
 
     @Override
     public void onFailure(Call<DocumentFeatures> call, Throwable t) {
-        Log.d("Error network call",t.toString());
+        Log.d("Error network call", t.toString());
     }
+
+
+
+    private class GetFeatureTask extends AsyncTask<String,Integer,DocumentFeatures> {
+
+        @Override
+        protected DocumentFeatures doInBackground(String... params) {
+            try {
+                SendAlchemyCallInBackground();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... progress) {
+            //setProgressPercent(progress[0]);
+        }
+
+        @Override
+        protected void onPostExecute(DocumentFeatures documentFeatures){
+
+        }
+    }
+
+    public void SendAlchemyCallInBackground(){
+
+        Document doc = null;
+        com.alchemyapi.api.AlchemyAPI api = null;
+
+        try
+        {
+            api = AlchemyAPI.GetInstanceFromString(AlchemyAPI_Key);
+        }
+        catch( IllegalArgumentException ex )
+        {
+            textView.setText("Error loading AlchemyAPI.  " +
+                    "Check that you have a valid AlchemyAPI key set in the AlchemyAPI_Key variable." +
+                    "Keys available at alchemyapi.com.");
+            return;
+        }
+        try {
+            AlchemyAPI_KeywordParams keywordParams = new AlchemyAPI_KeywordParams();
+            api.TextGetRankedKeywords(text_to_parse, keywordParams);
+
+        }catch (SAXException e) {
+            e.printStackTrace();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (XPathExpressionException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
 }
